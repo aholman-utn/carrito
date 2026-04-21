629a75bf10cbfa93395151989e16fb4b76212f22
Lucas <lpangaro@MacBook-de-Lucas.local>
2026-04-21 16:14:10 -0300

    clase direcciones

--- carrito/src/main/java/model/Direccion.java
+++ carrito/src/main/java/model/Direccion.java
@@ -3,6 +3,7 @@ package model;
 public class Direccion {
     private String calle1;
     private String calle2;
+
     private String altura;
     private boolean sinAltura;
     private String piso;
@@ -11,24 +12,32 @@ public class Direccion {
     private Ciudad ciudad;
 
     public String getDireccion(){
-        //FIXME: arreglar
-        String direccion = calle1 + calle2 + altura + piso+
-                cuerpo+ departamento + ciudad;
+        String direccion;
+        if (esUnaEsquina()) {
+             direccion = calle1 + calle2 + piso + cuerpo + departamento + ciudad; // Convengamos que si estoy en una esquina igual tengo una direccion con numero, y que si solo pongo el cruce de calles tengo 4 posibles esquinas
+        }
+        else direccion = calle1 + altura + piso + cuerpo + departamento + ciudad;
+       
         return direccion;
     }
+
+    public boolean esUnaEsquina(){
+        return(calle1 != null && calle2 != null);
+    }
+
     public String getLatitud(){
-        //FIXME: arreglar
+        //TODO: todavia no se usar la api de google para que me de la latitud, besis
         String latitud = "";
         return latitud;
     }
 
     public String getLongitud(){
-        //FIXME: arreglar
+        //TODO: todavia no se usar la api de google para que me de la longitud, besis
         String longitud = "";
         return longitud;
     }
     public boolean estaHabilitadaEnvio(){
-        //FIXME: arreglar
-        return false;
+        //No le tenemos asco a ningun lugar, respiramos barrio
+        return true;
     }
 }
